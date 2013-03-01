6cf3ae3e4eac5c61f32e6ee8b9c7a45042c178b5
Marie Gouaillardet <mgouaill@info-viciousx.iut.bx1>
2013-03-01 13:36:20 +0100

    modif de copier.c

--- copier.c
+++ copier.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 void copier(int argc, char **argv)
 {
 		FILE *fSrc;
 		FILE *fDest;
 		int nb=2;
 		char ligne[90];
+		int nb_ligne = 0;
 
 		if ((fDest= fopen(argv[argc-1],"w")) == NULL)
 		{
@@ -17,10 +17,15 @@ void copier(int argc, char **argv)
 		{			
 			if ((fSrc = fopen(argv[nb],"r")) == NULL)
 				fprintf(stderr, "\nErreur: Impossible de lire le fichier %s\n",argv[nb]);
-
+			
+			//nb_ligne = nombreLignes(fSrc);
+			
 			fgets(ligne, 90, fSrc);
 			fputs(argv[nb],fDest);
 			fputc('\n',fDest);
+			//char nbLigne[10];
+			//snprintf(nbLigne, 10, "%i", nb_ligne);
+			//fputs(nbLigne,fDest);
 
 			while (!feof(fSrc))
 			{
